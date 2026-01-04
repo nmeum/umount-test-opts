5e4d5b39de08457ecc841d5ed6d7b9633538c769
Sören Tempel <soeren+git@soeren-tempel.net>
2026-01-04 23:55:08 +0100

    klee_assume: Prevent compiler from short-circuiting
    See https://klee-se.org/docs/intrinsics/#interaction-with-short-circuit-operators

--- main.c
+++ main.c
@@ -16,12 +16,12 @@ main(int argc, char **argv)
 
 	klee_make_symbolic(&options, sizeof(options), "options");
 	for (int i = 0; i < sizeof(options) - 1; i++)
-		klee_assume(isalpha(options[i]) || options[i] == ',' || options[i] == '+');
+		klee_assume(isalpha(options[i]) | (options[i] == ',') | (options[i] == '+'));
 	klee_assume(options[sizeof(options) - 1] == '\0');
 
 	klee_make_symbolic(&pattern, sizeof(pattern), "pattern");
 	for (int i = 0; i < sizeof(pattern) - 1; i++)
-		klee_assume(isalpha(pattern[i]) || pattern[i] == ',' || pattern[i] == '+');
+		klee_assume(isalpha(pattern[i]) | (pattern[i] == ',') | (pattern[i] == '+'));
 	klee_assume(pattern[sizeof(pattern) - 1] == '\0');
 
 #if 0
